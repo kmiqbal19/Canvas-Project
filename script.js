f70d953a326756860e6d2e4c6fa073b74c9dc6f4
kmiqbal19 <km.iqbal01@gmail.com>
2022-07-02 15:31:39 +0200

    implemented circle animation for 100 circles

--- script.js
+++ script.js
@@ -33,26 +33,49 @@ const c = canvas.getContext("2d");
 // }
 
 // ANIMATION
-let x = Math.random() * innerWidth;
-let y = Math.random() * innerHeight;
-const radius = 30;
-let dx = (Math.random() - 0.5) * 10;
-let dy = (Math.random() - 0.5) * 5;
+function Circle(x, y, radius, dx, dy) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.dx = dx;
+  this.dy = dy;
 
+  this.draw = function () {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+
+    c.strokeStyle = "blue";
+    c.stroke();
+  };
+  this.update = function () {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+  };
+}
+let circle = new Circle(100, 150, 20, 3, 4);
+
+const circleArray = [];
+for (let i = 0; i < 100; i++) {
+  const radius = 30;
+  let x = Math.random() * (innerWidth - radius * 2) + radius;
+  let y = Math.random() * (innerHeight - radius * 2) + radius;
+  let dx = Math.random() - 0.5;
+  let dy = Math.random() - 0.5;
+  circleArray.push(new Circle(x, y, radius, dx, dy));
+}
+// console.log(circleArray);
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
-  c.beginPath();
-  c.arc(x, y, radius, 0, Math.PI * 2, false);
-  c.strokeStyle = "green";
-  c.stroke();
-  if (x + radius > innerWidth || x - radius < 0) {
-    dx = -dx;
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].draw();
+    circleArray[i].update();
   }
-  if (y + radius > innerHeight || y - radius < 0) {
-    dy = -dy;
-  }
-  x += dx;
-  y += dy;
 }
 animate();
